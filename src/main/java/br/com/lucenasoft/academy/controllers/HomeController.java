2f0b91cebd3031273168f9237e9a85294638bf4a
lucenasoft <getugbr@gmail.com>
2023-05-19 10:56:23 -0300

    feat: implements index and styles

--- src/main/java/br/com/lucenasoft/academy/controllers/HomeController.java
+++ src/main/java/br/com/lucenasoft/academy/controllers/HomeController.java
@@ -3,6 +3,7 @@ package br.com.lucenasoft.academy.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @RestController
 public class HomeController {
