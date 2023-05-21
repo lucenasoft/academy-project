16a631f291096ffe3bfca3681a7c87376ca45979
lucenasoft <getugbr@gmail.com>
2023-05-21 15:28:13 -0300

    feat: added methods: delete and alter student

--- src/main/java/br/com/lucenasoft/academy/controllers/StudentController.java
+++ src/main/java/br/com/lucenasoft/academy/controllers/StudentController.java
@@ -3,13 +3,11 @@ package br.com.lucenasoft.academy.controllers;
 import br.com.lucenasoft.academy.models.StudentModel;
 import br.com.lucenasoft.academy.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -39,6 +37,31 @@ public class StudentController{
         ModelAndView mv = new ModelAndView();
         mv.setViewName("student/listStudents");
         mv.addObject("studentslist", repository.findAll());
+        return mv;
+    }
+
+    @GetMapping("/alter/{id}")
+    public ModelAndView alterStudent(@PathVariable("id") UUID id){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("student/formAlter");
+        StudentModel student = repository.getReferenceById(id);
+        mv.addObject("student", student);
+        return mv;
+    }
+
+    @PostMapping("/alterStudent")
+    public ModelAndView alterStudent(StudentModel student) {
+        ModelAndView mv = new ModelAndView();
+        repository.save(student);
+        mv.setViewName("redirect:/list-students");
+        return mv;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView deleteStudent(@PathVariable("id") UUID id){
+        ModelAndView mv = new ModelAndView();
+        repository.deleteById(id);
+        mv.setViewName("redirect:/list-students");
         return mv;
     }
 }
